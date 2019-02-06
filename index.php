a8ac3989dc438ba6ec13a395874413c0dc50ada7
Alex Jose <alexjose4u@gmail.com>
2019-02-06 15:06:13 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$lists = [
+    ['id' => 1, 'label' => 'Item 1', 'parent_id' => 0, 'count' => 60],
+    ['id' => 2, 'label' => 'Item 2', 'parent_id' => 0, 'count' => 15],
+    ['id' => 3, 'label' => 'Item 3', 'parent_id' => 2, 'count' => 10],
+    ['id' => 4, 'label' => 'Item 4', 'parent_id' => 1, 'count' => 25],
+    ['id' => 5, 'label' => 'Item 5', 'parent_id' => 2, 'count' => 45],
+    ['id' => 6, 'label' => 'Item 6', 'parent_id' => 5, 'count' => 30],
+    ['id' => 7, 'label' => 'Item 7', 'parent_id' => 5, 'count' => 100],
+    ['id' => 8, 'label' => 'Item 8', 'parent_id' => 2, 'count' => 75],
+    ['id' => 9, 'label' => 'Item 9', 'parent_id' => 1, 'count' => 5],
+    ['id' => 10, 'label' => 'Item 10', 'parent_id' => 6, 'count' => 40],
+
+];
